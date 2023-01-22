e1eadc66f28fe38e319468246ff2a8e5065a920b
PBJTP <jplypick1990@gmail.com>
2023-01-21 20:20:09 -0800

    Finish functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('hello world');
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function isChecked() {
+    if (document.getElementById('read').checked) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function addBookToLibrary(event) {
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value;
+    read = isChecked();
+    myLibrary.push(new Book(title, author, pages, read));
+    document.getElementById('bookForm').reset();
+    event.preventDefault();
+}
+
+let addBtn = document.getElementById('bookForm');
+
+addBtn.addEventListener('submit', addBookToLibrary);
