50a6e3b253f78ebafe3fd936f28e6fd4e0e6bb5a
PBJTP <jplypick1990@gmail.com>
2023-01-23 18:44:48 -0800

    Add class option. Move isCHecked function to ternary

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.log('hello world');
-
 let myLibrary = [];
+
+// Factory Function way
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -9,19 +9,34 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
-function isChecked() {
-    if (document.getElementById('read').checked) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// Class way
+
+// class Book {
+//     constructor(title, author, pages, read) {
+//         this.title = title;
+//         this.author = author;
+//         this.pages = pages;
+//         this.read = read;
+//     }
+// }
+
+// Moved to Ternary within addBookToLibrary function
+
+// function isChecked() {
+//     if (document.getElementById('read').checked) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
 
 function addBookToLibrary(event) {
     title = document.getElementById('title').value;
     author = document.getElementById('author').value;
     pages = document.getElementById('pages').value;
-    read = isChecked();
+    read = document.getElementById('read').checked ? true : false;
     myLibrary.push(new Book(title, author, pages, read));
     document.getElementById('bookForm').reset();
     event.preventDefault();
