e70216c183ab4cb4aace5a8ca61edfb90947f92a
PBJTP <jplypick1990@gmail.com>
2023-01-28 00:40:27 -0800

    Fix some button size issues. shrink form modal Comment JS

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// generate a few placeholder books ONCE CODE BELOW IS DONE
+
+// add listener "add book"
+
+// show modal with book info form
+
+// create book with form data and add to myLibrary array
+
+// add book data to card, add style tags
+
+// add style tags, and value to each book based on array
+
+// append div
+
+// add listener to remove book button
+
+// remove book from array based on index/value
+
 let myLibrary = [];
 let addBtn = document.getElementById('bookForm');
 
